6d5e5c084caba75a4c586291fe02ab7437da5d41
Hema Bindra <hbindra11@yahoo.com>
2024-12-12 15:59:21 +0100

    included app router, showing a header with navbar,a homepage and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./components/general/Layout";
+import HomePage from "./components/general/HomePage";
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route element={<Outlet />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Route>
+    )
+  );
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
 
-export default App
+export default App;
