a7e5a954676ec264ef92660dfca93254e76f9b6b
Hema Bindra <hbindra11@yahoo.com>
2024-12-13 19:06:57 +0100

    can login through the login form

--- src/components/authentication/LoginForm.jsx
+++ src/components/authentication/LoginForm.jsx
@@ -1,23 +1,59 @@
+import { useForm } from "react-hook-form";
+import { useApp } from "../../context/AppContext";
+
 export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const { login } = useApp();
+
   return (
-    <form className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-md">
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form
+      className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-md"
+      onSubmit={handleSubmit(login)}
+    >
       <h2 className="text-2xl font-bold mb-4 text-center">Log In</h2>
 
       {/* Email Field */}
       <label className="input input-bordered flex items-center gap-2 mb-4">
         Email
-        <input type="email" className="grow" placeholder="yourname@example.com" />
+        {/* register your input into the hook by invoking the "register" function */}
+        <input
+          type="email"
+          className="grow"
+          placeholder="yourname@example.com"
+          name="email"
+          {...register("email", { required: true })}
+        />
+        {/* errors will return when field validation fails  */}
+        {errors.email && <span>This field is required</span>}
       </label>
 
       {/* Password Field */}
       <label className="input input-bordered flex items-center gap-2 mb-6">
         Password
-        <input type="password" className="grow" placeholder="Enter Password" />
+        {/* register your input into the hook by invoking the "register" function */}
+        <input
+          type="password"
+          className="grow"
+          placeholder="Enter Password"
+          name="password"
+          {...register("password", { required: true })}
+        />{" "}
+        {/* errors will return when field validation fails  */}
+        {errors.password && <span>This field is required</span>}
       </label>
 
-       {/* Forgot Password Link */}
-       <div className="flex justify-end mb-4">
-        <a href="/forgot-password" className="text-blue-500 hover:underline text-sm">
+      {/* Forgot Password Link */}
+      <div className="flex justify-end mb-4">
+        <a
+          href="/forgot-password"
+          className="text-blue-500 hover:underline text-sm"
+        >
           Forgot your Password?
         </a>
       </div>
--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -1,14 +1,29 @@
 import { useState } from "react";
-import { AppContext } from "./AppContext";
+import { AppContext } from "./AppContext.js";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const ContextProvider = ({ children }) => {
+function ContextProvider({ children }) {
   const [appUser, setAppUser] = useState(null); // logged-in user
+  const navigate = useNavigate();
+
+  const login = async (payload) => {
+    const {
+      data: { user, token },
+    } = await axios
+      .post(`${import.meta.env.VITE_API_SERVER}/auth/login`, payload)
+      .catch((error) => console.error(error));
+    setAppUser(user);
+    localStorage.setItem("token", token);
+    localStorage.setItem("appUser", JSON.stringify(appUser));
+    navigate("/");
+  };
 
   return (
-    <AppContext.Provider value={{ appUser, setAppUser }}>
+    <AppContext.Provider value={{ appUser, setAppUser, login }}>
       {children}
     </AppContext.Provider>
   );
-};
+}
 
 export default ContextProvider;
