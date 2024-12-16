6e29329bffd5777c543ed370a8c1723232217452
Gerd Braun <webmaster@your-d-sign.de>
2024-12-16 21:53:34 +0100

    added some routes, navbar changed, ...

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,40 @@ import SignupForm from "./components/authentication/SignupForm";
 import LoginForm from "./components/authentication/LoginForm";
 import ProjectList from "./components/projects/ProjectList";
 import ProjectDetail from "./components/projects/ProjectDetail";
+import ErrorPage from "./components/general/ErrorPage";
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/signUp" element={<SignupForm />} />
+        <Route path="/signup" element={<SignupForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/projects">
           <Route index element={<ProjectList />} />
           <Route path=":id" element={<ProjectDetail />} />
+          <Route path="add" element={<>ProjectAdd</>} />
+          <Route path="edit/:id" element={<>ProjectEdit</>} />
         </Route>
+        <Route path="/jobs">
+          <Route index element={<>JobList</>} />
+          <Route path=":id" element={<>JobDetail</>} />
+          <Route path="add" element={<>JobAdd</>} />
+          <Route path="edit/:id" element={<>JobEdit</>} />
+        </Route>
+        <Route path="/companies">
+          <Route index element={<>CompanyList</>} />
+          <Route path=":id" element={<>CompanyDetail</>} />
+          <Route path="add" element={<>CompanyAdd</>} />
+          <Route path="edit/:id" element={<>CompanyEdit</>} />
+        </Route>
+        <Route path="/users">
+          <Route index element={<>UserList</>} />
+          <Route path=":id" element={<>UserDetail</>} />
+          <Route path="add" element={<>UserAdd</>} />
+          <Route path="edit/:id" element={<>UserEdit</>} />
+        </Route>
+        <Route path="/*" element={<ErrorPage />} />
       </Route>
     )
   );
